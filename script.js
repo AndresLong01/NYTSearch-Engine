9fa829fdfa8f6cdf98eeb7dd4ce1560f171c0048
Andres Long <andreslong95@gmail.com>
2020-02-14 13:15:11 -0600

    small changes ready to test

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var searchBtn = $("button");
 
+var field1 = "lincoln";
+var beginDate = "2000";
+var endDate = "2019";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=nZxDzm1iq1RoTDQumxglBnkJh09PA3Tp";
+
 $.ajax({
     url: queryUrl,
     method: "GET"
+}).then(function(response){
+    console.log(response);
+    
 })
