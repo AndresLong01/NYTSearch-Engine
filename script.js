613ff31cbbac30cd6eebda72a692d331a7f3fe53
Andres Long <andreslong95@gmail.com>
2020-02-14 14:15:12 -0600

    Ajax works

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 var searchBtn = $("button");
+var selector = $("selectoring");
+var field1 = "coronavirus";
+var beginDate = "20000101";
+var endDate = "20191231";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+field1+"&begin_date="+ beginDate + "&end_date"+ endDate +"&api-key=nZxDzm1iq1RoTDQumxglBnkJh09PA3Tp";
 
-var field1 = "lincoln";
-var beginDate = "2000";
-var endDate = "2019";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=nZxDzm1iq1RoTDQumxglBnkJh09PA3Tp";
+var selection;
 
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-    
+selector.on("change", function(e){
+    e.preventDefault();
+    selection = parseInt(selector.val());
 })
+
+searchBtn.on("click", function(){
+    //Where is the sibling element that corresponds to the value of how many pages you want to see
+    //when that is selected, console log val();
+    //selection
+    //date
+    // hello
+    //button $(this).prev().prev().prev().val();
+    //use that value to change the loop for the result
+})
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        var responses = response.response.docs;
+        
+        for(i=0; i<responses.length; i++){
+            var newDiv = $("<div>");
+            newDiv.text(responses[i].abstract);
+            var newA = $("<a>");
+            newA.attr("href", responses[i].web_url);
+            newA.text("Link");
+            newDiv.append(newA);
+            $("body").append(newDiv);
+        }
+    });
